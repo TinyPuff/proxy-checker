c0442d1e4464f32dd587ca135d3dca622ad66ca8
Puff <MessageVan@protonmail.com>
2020-03-12 04:30:01 +0330

    Added a bunch of new features and fixed some stuff

--- filter.py
+++ filter.py
@@ -1,54 +1,72 @@
-# To-Do: 1- I should use threads to get the job done as quickly as possible.
-# 2- Allow the user to pick their own input and output file.
-# 3- I should add the comments into the README file.
-# 4- Make this work for the socks proxies too.
 import requests as req
-# from fake_useragent import UserAgent
 
 
-def filter():  # Puts the proxies that work in a file named "result.txt"
-    print("Note that every time you check the quality of these proxies, the previous proxies"
-          + " saved in result.txt\n" + "will be deleted. You may want to copy them and save them in another "
-          + "text file")
+def filter():
+    """ Puts the proxies that work in a file named 'result.txt' """
+
+    print(
+        '''Note that every time you check the quality of these proxies, the previous proxies saved in output file will be deleted.
+You may want to copy them and save them in another text file''')
+
+    global default_input
+    default_input = "IO/default.txt"
+    global default_output
+    default_output = "IO/result.txt"
+    protocols = ["HTTP", "HTTPS", "SOCKS5"]
+    for i in protocols:
+        print("-", i)
     while True:
-        make_sure = input("Are you sure you want to do this?(y/n)\n> ").lower()
-        if make_sure in ["y", "yes"]:
-            print("Here we go...")
-            result = open('IO/result.txt', 'w')
+        print("What protocol are your target proxies using?")
+        proto = input("> ").upper()
+        if proto in protocols:
             break
-        elif make_sure in ["n", "no"]:
-            print("Ciao!")
-            return 0
+    while True:
+        try:
+            choice = int(input(
+                "Enter 0 for using custom input/output files and enter 1 for using the default ones.\n> "))
+            if choice == 1:
+                result = open(default_output, 'w')
+                proxy_list = open(default_input)
+                break
+            elif choice == 0:
+                proxy_list = open(
+                    input("Enter the path to the input file: (e.g. C:/Downloads/proxies.txt)\n> "))
+                result = open(
+                    input("Enter the path to the output file: (e.g. C:/Downloads/result.txt)\n> "),
+                    'w')
+                break
+        except (ValueError, FileNotFoundError):
+            pass
 
-    proxy_list = open('IO/default.txt')
     for proxy in proxy_list:
         success = []
         fail = []
-        # ua = UserAgent()
-        # user_agent = ua.random
-        # headers = {
-        #    "User-Agent": user_agent
-        # }
-        proxy_dict = {
-            "http": f"http://{proxy}",
-            "https": f"http://{proxy}"
-        }
+        if proto in ("HTTP", "HTTPS"):
+            proxy_dict = {
+                "http": f"http://{proxy}",
+                "https": f"http://{proxy}"
+            }
+        elif proto == "SOCKS5":
+            proxy_dict = {
+                "http": f"socks5://{proxy}",
+                "https": f"socks5://{proxy}"
+            }
         target = '<img alt="Google"'
         bw = 7
         for i in range(3):
             if 1 in success:
                 break
             try:
-                resp1 = req.get("https://www.google.com", proxies=proxy_dict, timeout=bw)
+                resp1 = req.get(
+                    "https://www.google.com",
+                    proxies=proxy_dict,
+                    timeout=bw)
 
                 if target in resp1.text:
-                    # print(f"{proxy}: Successful!")
                     print("{}".format(proxy.strip('\n')))
                     success.append(1)
             except (req.exceptions.ProxyError, req.exceptions.ConnectTimeout, req.exceptions.ConnectionError):
-                # print(f"{proxy}: Failure!")
                 fail.append(0)
-                pass
             bw -= 3
 
         if 1 in success:
@@ -56,11 +74,13 @@ def filter():  # Puts the proxies that work in a file named "result.txt"
     result.close()
 
 
-def dtbl():  # Deletes the blank lines
+def dtbl():
+    """ Deletes the blank lines """
+
     try:
-        with open('IO/result.txt', 'r') as f:
-            lines = f.readlines()
-        with open('IO/result.txt', 'w') as f:
+        with open(default_output, 'r') as f:
+            lines = [line for line in f]
+        with open(default_output, 'w') as f:
             for line in lines:
                 if line.strip("\n") != "":
                     f.write(line)
@@ -68,7 +88,12 @@ def dtbl():  # Deletes the blank lines
         pass
 
 
-if __name__ == "__main__":
+def main():
+    """ Executes everything """
     a = filter()
     if a != 0:
         dtbl()
+
+
+if __name__ == "__main__":
+    main()
